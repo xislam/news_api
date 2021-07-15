024f8ee0fe6cc2d5107688dd7bf0ffaae7731dde
xislam <nurdinovislam4@gmail.com>
2021-07-16 01:44:57 +0600

    создан проект настроен DRF, JWT сделаны создания и аутентификация обновления токена токены сохраняются в базе следуйший шак это новости

---
+++ api_news_user/views.py
@@ -0,0 +1,21 @@
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from .serializers import UserSerializer
+
+
+@api_view(['POST'])
+def create_user(request):
+    serializer = UserSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(
+            {'data': serializer.data},
+            status=status.HTTP_201_CREATED
+        )
+
+    return Response(
+        {'data': serializer.errors},
+        status=status.HTTP_400_BAD_REQUEST
+    )
